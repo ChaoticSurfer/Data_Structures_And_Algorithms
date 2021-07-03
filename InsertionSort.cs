e7ae466c18242ced6ec28f99e23247345229e4bf
Anri Kezeroti <48965494+ChaoticSurfer@users.noreply.github.com>
2021-07-03 20:43:38 +0400

    Update InsertionSort.cs

--- InsertionSort.cs
+++ InsertionSort.cs
@@ -1,55 +1,137 @@
 using System;
 
-namespace ConsoleApp1
+namespace Algorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var arr = Insertion.Sort(new int[] {1, 25, 5, 5, 5, 3, 4,});
-
-            foreach (var VARIABLE in arr)
-            {
-                Console.WriteLine(VARIABLE);
-            }
+            var arr = new[] {12, 11, 13, 5, 6, 7};
+            printArray(arr);
+            Quick.Sort(arr);
+            printArray(arr);
         }
 
-        static class Insertion
+        public static void printArray(int[] arr)
         {
-            public static T[] Sort<T>(T[] array) where T : System.IComparable<T>
+            foreach (var i in arr)
             {
-                int n = array.Length;
-
-                for (int i = 1; i < n; i++)
-                {
-                    int j = i;
-                    while (j > 0 && array[j - 1].CompareTo(array[j]) == 1)
-                    {
-                        swap(array, j, j-1);
-                        --j;
-                    }
-                }
-                return array;
-
+                Console.Write($"{i} ");
             }
+
+            Console.WriteLine();
+        }
+    }
+
+    public static class Quick
+    {
+        private static int CUT_OFF = 10;
+
+        public static void Sort<T>(T[] array) where T : IComparable
+        {
+            KnuthShuffle(array);
+            Quick.Sort(array, 0, array.Length - 1);
+        }
+
+        private static void Sort<T>(T[] array, int lo, int hi) where T : IComparable
+        {
+            if (hi - lo > CUT_OFF) Insertion.Sort(array);// cut off for small sub arrays
+            
+            if (hi <= lo)
+                return;
+            int j = Partition(array, lo, hi);
+            Sort(array, lo, j - 1);
+            Sort(array, j + 1, hi);
+        }
+
+        // UTILITY FUNCTIONS
+
+        public static int Partition<T>(T[] array, int lo, int hi) where T : IComparable
+        {
+            int i = lo;
+
+            int j = hi + 1;
+            var v = array[lo];
+            while (true)
+            {
+                while (less(array[++i], v))
+                    if (i == hi)
+                        break;
+
+
+                while (less(v, array[--j])) ;
+                // if (j == lo)
+                //     break;
+
+                if (i >= j)
+                    break;
+
+                swap(array, i, j);
+            }
+
+            swap(array, j, lo);
+
+            // now, a[lo .. j-1] <= a[j] <= a[j+1 .. hi]
+            return j;
+        }
+
+
+        private static bool less(IComparable i, IComparable j)
+        {
+            return i.CompareTo(j) == -1;
+        }
+
+        private static T[] KnuthShuffle<T>(T[] array)
+        {
+            var random = new Random();
+            var n = array.Length;
+            for (int i = 0; i < n; i++)
+            {
+                var randomNumber = random.Next(0, n);
+                swap(array, i, randomNumber);
+            }
+
+            return array;
         }
 
         private static void swap<T>(T[] array, int i, int j)
         {
             if (i == j) return;
+
             T temp;
             temp = array[i];
             array[i] = array[j];
             array[j] = temp;
         }
+    }
 
-        private static void swapNums(int[] array, int i, int j)
+    static class Insertion
+    {
+        public static T[] Sort<T>(T[] array) where T : System.IComparable
+        {
+            int n = array.Length;
+
+            for (int i = 1; i < n; i++)
+            {
+                int j = i;
+                while (j > 0 && array[j - 1].CompareTo(array[j]) == 1)
+                {
+                    swap(array, j, j - 1);
+                    --j;
+                }
+            }
+
+            return array;
+        }
+
+        private static void swap<T>(T[] array, int i, int j)
         {
             if (i == j) return;
-            array[i] ^= array[j];
-            array[j] ^= array[i];
-            array[i] ^= array[j];
+
+            T temp;
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
     }
 }
-
